042cf21e20c86632794486153a4165a265a93d17
CFdefense <152815652+CFdefense@users.noreply.github.com>
2025-03-04 15:57:53 -0500

    Fix old methods

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: {
+    files: ['./src/**/*.{html,js,svelte,ts}'],
+    transform: {
+      svelte: (content) => {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+      }
+    }
+  },
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography')
-  ],
-  // Add purge options to handle Svelte files properly
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.svelte'],
-    options: {
-      safelist: [],
-      defaultExtractor: (content) => {
-        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
-        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
-      },
-    },
-  },
+  ]
 } 
