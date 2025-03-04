2612371ed2bc6f23a3b460cb8367e37613c0a7a6
CFdefense <152815652+CFdefense@users.noreply.github.com>
2025-03-04 16:03:47 -0500

    Setup backend project structure

---
+++ backend/src/db/pool.rs
@@ -0,0 +1,4 @@
+// src/db/pool.rs
+
+// Please use sqlx to create a connection pool to the database
+// Use the DATABASE_URL environment variable to connect to the database
---
+++ backend/src/models/user.rs
@@ -0,0 +1,3 @@
+// src/models/user.rs
+
+// Define Application User Model to be used in the application and in the database
