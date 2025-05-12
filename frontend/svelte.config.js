d78b4f14c7ee69d4e04f87b62078c6b3b475d689
CFdefense <CFdefence@gmail.com>
2025-05-12 19:04:43 -0400

    new push

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {adapter: adapter()}
+    kit: {
+        adapter: adapter({
+            // set output to match Render's expected path
+            pages: '.svelte-kit/output/client',
+            assets: '.svelte-kit/output/client',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
+        })
+    }
 };
 
 export default config;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config({ path: '../backend/.env' });
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	plugins: [sveltekit()],
-	// Define global constants - effectively replaces process.env.API_BASE_URL
-	define: {
-		'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL)
-	},
-	css: {
-		devSourcemap: false
-	},
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			// Set output to match Render's expected path
-			pages: '.svelte-kit/output/client',
-			assets: '.svelte-kit/output/client',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
-	}
-};
-
-export default defineConfig(config);
+export default defineConfig({
+    plugins: [sveltekit()],
+    // Define global constants - effectively replaces process.env.API_BASE_URL
+    define: {
+        'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL)
+    },
+    css: {
+        devSourcemap: false
+    }
+});
