c93fb9c0a070629b61db3ba5dc49763756d799f9
CFdefense <CFdefence@gmail.com>
2025-05-14 08:46:44 -0400

    moved to static

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -116,7 +116,7 @@
 							  muted
 							  playsinline
 							>
-							  <source src="/src/lib/assets/video.mp4" type="video/mp4" />
+							  <source src="/video.mp4" type="video/mp4" />
 							  Your browser does not support the video tag.
 							</video>
 						  </div>
